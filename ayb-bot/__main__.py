0f2130b4f991bf07465de898c5b943d9c17e45b6
Colin Schatz <cschatz@mills.edu>
2022-02-28 20:26:26 -0800

    Refactor further

--- ayb-bot/__main__.py
+++ ayb-bot/__main__.py
@@ -1,61 +1,8 @@
-#!/usr/bin/env python3
-
-import discord
-from quotations import FactQuotes, MemeQuotes
+from bot import AllYourBASEBot
 
 # Bad practice! Needs to eventually
 # move to somewhere else local
 TOKEN = "OTQwNDgxODgyOTEzNjQwNTA4.YgICAg.uecQGHPZq1TU5TJUZ7xfE3cewg4"
 
-client = discord.Client()
-
-facts = FactQuotes()
-memes = MemeQuotes()
-
-
-async def show_help(channel):
-    info = "Currently, I am barely implemented.\n"\
-        "Commands include:\n"\
-        "`.base fact` - request a fact\n"\
-        "`.base meme` - request a meme"
-    await channel.send(info)
-
-
-async def respond(channel, query):
-    if not query:
-        await show_help(channel)
-    else:
-        if query == "fact":
-            await channel.send("***" + facts.next() + "***")
-        elif query == "meme":
-            await channel.send("***" + memes.next() + "***")
-        else:
-            await show_help(channel)
-
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    listening_activity = discord.Activity(
-        type=discord.ActivityType.listening,
-        name=".base"
-    )
-    await client.change_presence(activity=listening_activity)
-
-
-@client.event
-async def on_message(message):
-    global index
-
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('.base'):
-        if len(message.content) == 5:
-            # ".base" entered alone
-            await show_help(message.channel)
-        elif message.content[5] == " ":
-            # ".base [somethinng]"
-            await respond(message.channel, message.content[6:])
-
+client = AllYourBASEBot()
 client.run(TOKEN)
---
+++ ayb-bot/bot/__init__.py
@@ -0,0 +1,51 @@
+import discord
+
+from quotation.collections import (
+    FactQuotes,
+    MemeQuotes,
+)
+
+
+class AllYourBASEBot(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.facts = FactQuotes()
+        self.memes = MemeQuotes()
+
+    async def on_ready(self):
+        print(f"We have logged in as {self.user}")
+        listening_activity = discord.Activity(
+            type=discord.ActivityType.listening,
+            name=".base"
+        )
+        await self.change_presence(activity=listening_activity)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('.base'):
+            if len(message.content) == 5:
+                # ".base" entered alone
+                await self._show_help(message.channel)
+            elif message.content[5] == " ":
+                # ".base [somethinng]"
+                await self._respond(message.channel, message.content[6:])
+
+    async def _respond(self, channel, query):
+        if not query:
+            await self._show_help(channel)
+        else:
+            if query == "fact":
+                await channel.send("***" + self.facts.next() + "***")
+            elif query == "meme":
+                await channel.send("***" + self.memes.next() + "***")
+            else:
+                await self._show_help(channel)
+
+    async def _show_help(self, channel):
+        info = "Currently, I am just an experiment.\n"\
+            "Commands include:\n"\
+            "`.base fact` - request a fact\n"\
+            "`.base meme` - request a meme"
+        await channel.send(info)
